03da5d7c52fd90edf7aaa133684c8b7a0d43570f
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-04-27 13:48:40 -0300

    feat: Adds Manhattan distance calculation functionality
    Implements a function to calculate the Manhattan distance between points in a matrix.
    
    Includes a sample matrix and error handling in the main execution flow.
    
    Improves user feedback by printing the calculated distance or error messages.

--- main.go
+++ main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"teste_zarv/manhattan"
+)
+
+func manhattanDistance(matrix [][]int) (int, error) {
+	return manhattan.Distance(matrix)
+}
+
+func run() error {
+	matrix := [][]int{
+		{0, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+	}
+
+	distance, err := manhattanDistance(matrix)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("A distância de Manhattan entre os dois pontos é: %d\n", distance)
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("Erro: %v\n", err)
+	}
+}
